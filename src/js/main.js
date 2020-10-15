e6f95ed294eefc9323d3e714fb41b5c294b524cb
Maxim Right <maxright1984@gmail.com>
2020-10-15 13:01:03 +0300

    Мелкий фикс кнопки

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 var btn = $('#top__button');
 
 $(window).scroll(function () {
-  if ($(window).scrollTop() > 2500) {
+  if ($(window).scrollTop() > 2420) {
     btn.addClass('show');
   } else {
     btn.removeClass('show');
